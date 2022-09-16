45c5754af307e6f1960e544e2db0afb37e2818fb
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-09-16 15:27:55 +0000

    Taking in the value for calculation

--- .ccls-cache/@home@runner@CSCI130Week4HWDillonHendrix/HW_P3@main3.cpp
+++ .ccls-cache/@home@runner@CSCI130Week4HWDillonHendrix/HW_P3@main3.cpp
@@ -1,5 +1,35 @@
+/**** Last Factorial *****/
+
+
 #include <iostream>
+using namespace std;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  // Read in # of test cases
+  int num_cases;
+  cin >> num_cases;
+
+  // Declare values for the loop
+  int fac_val;
+  
+  // Make a loop for test cases
+  for (int i = 0; i < num_cases; i++) {
+  // take in a value for calculation
+  cin >> fac_val;
+
+  // make this calculation
+
+
+  // find the value of the last digit (%10)
+
+
+  // output the last digit 
+
+    
+  }
+
+  
+
+  
 }
--- HW_P5/main5.cpp
+++ HW_P5/main5.cpp
@@ -1,5 +1,19 @@
+/***** Cannon Ball Problem ******/
+
 #include <iostream>
+using namespace std;
+#include <iomanip>
 
 int main() {
-  std::cout << "Hello World!\n";
+  // Read in line 1. Tells how many test cases.
+  int n;
+  cin >> n;
+
+
+  
+  // Vars for loop
+  double v0, x1, degree, h1, h2; 
+  
+  
+  
 }
